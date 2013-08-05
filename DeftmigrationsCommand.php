fedb00d703ab6ab093bbfc2ff3f5dd688a1bfb92
Varun Batra <codevarun@gmail.com>
2013-08-06 03:29:28 +0530

    Added actual file

---
+++ DeftmigrationsCommand.php
@@ -0,0 +1,343 @@
+<?php
+
+Yii::import('system.cli.commands.MigrateCommand');
+
+/**
+ * Class        DeftmigrationsCommand
+ *
+ * @description Dump table schema or data depending on functions used. This file is meant to use with Yii 1.1.13+. Currently it only supports MySQL since when I wrote it, I was working on project with MySQL database.
+ * usage:
+ * @tutorial README.md
+ *
+ * @author      Varun Batra <codevarun@gmail.com>
+ * @copyright   Varun Batra
+ * @since       5/8/13
+ * @version     0.01
+ * @todo Make a multi insert compatible, as of today, Yii doesn't support it.
+ * @link  http://varunbatra.com/ Personal Website
+ * @link  http://deftinfotech.com/ Company Website
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+class DeftmigrationsCommand extends MigrateCommand
+{
+
+    public $table, $database, $fks;
+    public $skipTables = array();
+
+    public function beforeAction($action, $params)
+    {
+        if (parent::beforeAction($action, $params)) {
+            $this->skipTables[] = $this->migrationTable;
+            return true;
+        }
+    }
+
+    private function getDBName()
+    {
+        $curdb = explode('=', Yii::app()->db->connectionString);
+        return $curdb[2];
+    }
+
+    private function getCharacterSet($tableName)
+    {
+        $curdb = $this->getDBName();
+        $results = Yii::app()->db->createCommand('
+            SELECT  T.table_collation,CCSA.character_set_name FROM information_schema.`TABLES` T,
+            information_schema.`COLLATION_CHARACTER_SET_APPLICABILITY` CCSA
+            WHERE CCSA.collation_name = T.table_collation
+            AND T.table_schema = "' . $curdb . '"
+            AND T.table_name = "' . $tableName . '";
+            SHOW CHARACTER SET FOR ' . $curdb . '.' . $tableName
+                )->queryRow();
+        return $results;
+    }
+
+    private function promptCommon($name = false, $tableName = null)
+    {
+
+        $All = false;
+        if ($name == false) {
+            $name = $this->prompt('Can you please at least tell me name of Migration?');
+            if ($name == "") {
+                echo "\nExiting No valid name is given\n";
+                exit(1);
+            }
+        }
+        if ($tableName == null) {
+            $All = $this->prompt('Ok so you didn\'t give me table name, should I consider all tables. It may eat up your memory? yes/no ', 'no');
+            $All = trim(strtolower($All));
+            if ($All != 'no' && $All != 'yes') {
+                echo "\nExiting No valid answer is given\n";
+                exit(1);
+            }
+        }
+
+        if ($All == 'no') {
+            $tableName = $this->prompt('Please tell me table name');
+        } elseif ($All == 'yes') {
+            $tableName = true;
+        }
+        return array($name, $tableName);
+    }
+
+    public function actionTableSchema($name = false, $tableName = null)
+    {
+        list($name, $tableName) = $this->promptCommon($name, $tableName);
+        $tables = array();
+        if ($tableName === true) {
+            $tables = Yii::app()->db->schema->getTables();
+        } else {
+            $tables = Yii::app()->db->schema->getTable($tableName);
+            if (!$tables) {
+                echo "\nSorry table name is invalid\n";
+                exit(1);
+            }
+            $tables = array($tables);
+        }
+
+        $this->interactive = false;
+        foreach ($tables as $table) {
+            if (in_array($table->name, $this->skipTables)) {
+                continue;
+            }
+            $result = $this->getTableStructure($table);
+            $this->templateFile = $this->template($result);
+            $this->actionCreate(array($name . '_' . $table->name));
+        }
+        if (count($this->fks) > 0) {
+            echo "\n" . 'Waiting for 2 seconds to write Fks' . "\n";
+            sleep(2);
+        }
+        foreach ($this->fks as $table => $Fks) {
+            if ($Fks == '') {
+                continue;
+            }
+            $this->templateFile = $this->template($Fks);
+            $this->actionCreate(array($name . '_' . $table . 'Fks'));
+        }
+    }
+
+    public function actionTableData($name = false, $tableName = null)
+    {
+
+        list($name, $tableName) = $this->promptCommon($name, $tableName);
+
+        $AllTables = array();
+        $dumpTable = '';
+
+        $tables = Yii::app()->db->schema->getTables();
+        foreach ($tables as $table) {
+            if (in_array($table->name, $this->skipTables)) {
+                continue;
+            }
+            $AllTables[] = $table->name;
+        }
+        if ($tableName !== true) {
+            if (in_array($tableName, $AllTables)) {
+                $AllTables = array();
+                $AllTables[] = $tableName;
+            }
+        }
+        $this->interactive = false;
+        foreach ($AllTables as $table) {
+            $dumpTable = $this->getTableDump($table);
+            if ($dumpTable === false) {
+                continue;
+            }
+            $this->templateFile = $this->template($dumpTable);
+            $this->actionCreate(array($name . '_' . $table));
+        }
+    }
+
+    public function actionTableDump($name = false, $tableName = null)
+    {
+        list($name, $tableName) = $this->promptCommon($name, $tableName);
+        $this->actionTableSchema($name, $tableName);
+        echo "\n" . 'Sleeping for 2 seconds before getting their Data' . "\n";
+        sleep(2);
+        $this->actionTableData($name, $tableName);
+    }
+
+    private function getColType($col, $findKey = true)
+    {
+        if ($col->isPrimaryKey && $findKey) {
+            return "pk";
+        }
+        $result = $col->dbType;
+        if (!$col->allowNull) {
+            $result .= ' NOT NULL';
+        }
+        if ($col->defaultValue != null) {
+            $result .= " DEFAULT '{$col->defaultValue}'";
+        }
+        if ($col->autoIncrement) {
+            $result .= " AUTO_INCREMENT";
+        }
+        return $result;
+    }
+
+    private function findConstraints($table)
+    {
+        if (!isset($this->fks[$table->name])) {
+            $this->fks[$table->name] = '';
+        }
+        $i = 0;
+        $fkName = $this->getFkNames($table->name);
+        $Constraints = $this->findConstraintsMysql($table);
+        foreach ($table->foreignKeys as $key => $value) {
+            $onDelete = (isset($Constraints[0][$i]) && $Constraints[0][$i] != '') ? ($Constraints[0][$i]) : NULL;
+            $onUpdate = (isset($Constraints[1][$i]) && $Constraints[1][$i] != '') ? ($Constraints[1][$i]) : NULL;
+            if ($onDelete == NULL && $onDelete == Null) {
+                $this->fks[$table->name] .= '$this->addForeignKey("' . $fkName[$i] . '", "' . $table->name . '", "' . $key . '", "' . $value[0] . '", "' . $value[1] . '");' . "\n";
+            } else {
+                $this->fks[$table->name] .= '$this->addForeignKey("' . $fkName[$i] . '", "' . $table->name . '", "' . $key . '", "' . $value[0] . '", "' . $value[1] . '","' . $onDelete . '","' . $onUpdate . '");' . "\n";
+            }
+            $i++;
+        }
+    }
+
+    private function getFkNames($table)
+    {
+        return Yii::app()->db->createCommand("SELECT constraint_name FROM information_schema.table_constraints WHERE constraint_type = 'FOREIGN KEY' AND table_schema = DATABASE() AND table_name = '$table'")->queryColumn();
+    }
+
+    private function findConstraintsMysql($table)
+    {
+        $onDelete = array();
+        $onUpdate = array();
+        $j = $i = 0;
+        $row = Yii::app()->db->createCommand('SHOW CREATE TABLE ' . $table->rawName)->queryRow();
+        $matches = array();
+        $regexp = '/FOREIGN KEY\s+\(([^\)]+)\)\s+REFERENCES\s+([^\(^\s]+)\s*\(([^\)]+)\) ON DELETE (.*) ON UPDATE (.*),/mi';
+        foreach ($row as $sql) {
+            if (preg_match_all($regexp, $sql, $matches, PREG_SET_ORDER))
+                break;
+        }
+        foreach ($matches as $match) {
+            $onDelete[$i++] = $match[4];
+            $onUpdate[$j++] = $match[5];
+        }
+        return array($onDelete, $onUpdate);
+    }
+
+    private function getTableDump($table)
+    {
+        $tableDump = "\n" .
+                '$i=0;' .
+                "\n" .
+                '$sql = \'SET foreign_key_checks = 0\';' .
+                "\n" .
+                'Yii::app()->db->createCommand($sql)->execute();' .
+                "\n" .
+                '$sql = \'SET foreign_key_checks = 1\';' .
+                "\n" .
+                "Try{" .
+                "\n";
+
+        $data = Yii::app()->db->createCommand()->select()->from($table)->queryAll();
+        if (!$data) {
+            return false;
+        }
+        $tableDump .= "\n" . "/* Dumpling Structure for $table*/" . "\n";
+        foreach ($data as $dataOne) {
+            $tableDump .= "\n" . '$i++;' . "\n";
+            foreach ($dataOne as $columns => $value) {
+                $value = str_replace("'", "\'", $value);
+                $value = str_replace("\\'", "'", $value);
+                $tableDump .= '$toInsert[$i][\'' . $columns . '\'] = \'' . $value . '\'; ' . "\n";
+            }
+        }
+        $tableDump .= '
+foreach($toInsert as $insertRow){ ' . "\n" .
+                '$this->insert(\'' . $table . '\', $insertRow);' . "\n" . '
+}' . "\n\n" . '
+unset($toInsert);
+        ' . "\n" .
+                '}Catch(Exception $e){' .
+                "\n" .
+                'Yii::app()->db->createCommand($sql)->execute();' .
+                "\n" .
+                'echo \'Sorry got some error\';' .
+                "\n" .
+                'return false;' .
+                "\n" .
+                "}" .
+                "\n" .
+                'Yii::app()->db->createCommand($sql)->execute();' .
+                "\n" .
+                "\n";
+
+        return $tableDump;
+    }
+
+    private function getTableStructure($def)
+    {
+
+        $character = $this->getCharacterSet($def->name);
+        $result = '';
+        //Skipping Migrations
+        if ($def->name == 'migration') {
+            return false;
+        }
+        if (is_array($def->primaryKey)) {
+            $findKey = false;
+        } else {
+            $findKey = true;
+        }
+        if (isset($def->primaryKey)) {
+            $findKey = false;
+        }
+        if (isset($def->primaryKey) && !is_array($def->primaryKey)) {
+            $primary_keys = array($def->primaryKey);
+        }
+        if (isset($def->primaryKey) && is_array($def->primaryKey)) {
+            $primary_keys = $def->primaryKey;
+        }
+
+        $result .= '$this->createTable("' . $def->name . '", array(' . "\n";
+        foreach ($def->columns as $col) {
+            $result .= '    "' . $col->name . '"=>"' . $this->getColType($col, $findKey) . '",' . "\n";
+        }
+        if ($findKey === false && is_array($primary_keys)) {
+            $result .= '"PRIMARY KEY (' . implode(',', $primary_keys) . ')"';
+        }
+        $result .= '), " DEFAULT CHARACTER SET ' . $character['character_set_name'] . ' COLLATE ' . $character['table_collation'] . '");' . "\n\n";
+        $this->findConstraints($def);
+        return $result;
+    }
+
+    private function template($up = "")
+    {
+        $file = <<<EOD
+<?php
+
+class {ClassName} extends CDbMigration
+{
+	public function up()
+	{
+	$up
+	}
+
+	public function down()
+	{
+		echo "{ClassName} does not support migration down.\\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
+EOD;
+        file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'template.php', $file);
+        return 'application.commands.template';
+    }
+
+}
--- README.md
+++ README.md
@@ -1,4 +1,72 @@
 Deftmigrations
 ==============
 
-This reduces manual work of Yii developer by generating table schema and table data.
+This reduces manual work of Yii developer by generating migrations containing table schema and/or table data.
+
+You can entirely replace Yii default migration and use this one. In case you don't want to do it, just copy this class in protected\commands folder. It should be available automatically.
+
+##Following commands are available:
+```
+yiic.bat deftmigrations TableSchema
+```
+
+Above command will create migration with Table Schema (definition)
+
+```
+yiic.bat deftmigrations TableData
+```
+
+Above command will create migration with Table Data
+
+```
+yiic.bat deftmigrations TableDump
+```
+
+Above command will create migration with Table Schema and Table Data.
+
+
+##Passing Variables
+Although it will prompt for anything is needed but you can pass name of migration (which is used as prefix) and table name in command line like this:
+
+```
+yiic.bat deftmigrations TableDump  --name=DEFT --tableName=countries
+```
+
+Above command will create migration with Table Schema and Table Data for countries table.
+
+
+##Batch Command For All Tables
+
+If you don't pass any Table name then it will ask for considring all tables, type 'yes' on prompt. Just use any of following:
+
+```
+yiic.bat deftmigrations TableSchema
+```
+```
+yiic.bat deftmigrations TableData
+```
+```
+yiic.bat deftmigrations TableDump
+```
+
+
+##Please Note:
+ I have encountered many problems and eliminate it all like composite key, primary key was default as int(11) and many more. This class should be able to manage most of common databases & table. There is no guarantee that it will work for you.
+
+ To avoid Fks error, I have disabled its' check. In error it only displays that Migration got error and then I have re-enabled check of FK whether or not migration was successful or not.
+ To avoid weird sequence causing FKs error, there is a 2 seconds delay.
+ 
+##Migrations Name
+ 
+ Sample migration name when Name is giving as DEFT
+ 
+ m130805_190610_DEFT_cities
+ 
+ m130805_190610_DEFT_countries
+ 
+ Where cities and countries are tables
+ 
+ Thus migration name given is used as prefix with tables, Separate file was important to avoid memory problems during creation. You may still get memory problem during applying migrations.
+ 
+##Important
+ Currently for each insert, one query is executing. Yii as of 1.1.13 doesn't support multi-insertion but hopefully next release does. So you might end up many queries running. Well, this is for one time only. 
