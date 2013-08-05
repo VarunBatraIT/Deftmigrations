d6d2fd81b30861102fbe8e80c312a7af206b1ef2
Varun Batra <codevarun@gmail.com>
2013-08-06 03:34:27 +0530

    Added sample output

---
+++ sample/m130805_220313_DEFT_table_deft.php
@@ -0,0 +1,32 @@
+<?php
+
+class m130805_220313_DEFT_table_deft extends CDbMigration
+{
+	public function up()
+	{
+	$this->createTable("table_deft", array(
+    "id"=>"int(2) NOT NULL",
+    "id2"=>"int(11) NOT NULL AUTO_INCREMENT",
+    "text"=>"varchar(230)",
+"PRIMARY KEY (id,id2)"), " DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci");
+
+
+	}
+
+	public function down()
+	{
+		echo "m130805_220313_DEFT_table_deft does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
---
+++ sample/m130805_220317_DEFT_table_deft.php
@@ -0,0 +1,60 @@
+<?php
+
+class m130805_220317_DEFT_table_deft extends CDbMigration
+{
+	public function up()
+	{
+	
+$i=0;
+$sql = 'SET foreign_key_checks = 0';
+Yii::app()->db->createCommand($sql)->execute();
+$sql = 'SET foreign_key_checks = 1';
+Try{
+
+/* Dumpling Structure for table_deft*/
+
+$i++;
+$toInsert[$i]['id'] = '1'; 
+$toInsert[$i]['id2'] = '1'; 
+$toInsert[$i]['text'] = 'text2'; 
+
+$i++;
+$toInsert[$i]['id'] = '1'; 
+$toInsert[$i]['id2'] = '2'; 
+$toInsert[$i]['text'] = 'text1'; 
+
+foreach($toInsert as $insertRow){ 
+$this->insert('table_deft', $insertRow);
+
+}
+
+
+unset($toInsert);
+        
+}Catch(Exception $e){
+Yii::app()->db->createCommand($sql)->execute();
+echo 'Sorry got some error';
+return false;
+}
+Yii::app()->db->createCommand($sql)->execute();
+
+
+	}
+
+	public function down()
+	{
+		echo "m130805_220317_DEFT_table_deft does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
